69e0f63ca7d14969f93a47deea6f322c7cf74866
Kacper Fraszkiewicz <kacper.fraszkiewicz@gmail.com>
2024-05-07 20:03:19 +0200

    Bottom sheet layout for food filtering

--- app/src/main/java/com/example/foodapp/ClientScreens/Menu.kt
+++ app/src/main/java/com/example/foodapp/ClientScreens/Menu.kt
@@ -3,6 +3,7 @@ package com.example.foodapp.ClientScreens
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,19 +13,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,17 +45,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.foodapp.AdminScreens.DetailedFoodPropertiesScreen
+import com.example.foodapp.ClientPanel
 import com.example.foodapp.DBHandler
 import com.example.foodapp.Foods
 
@@ -51,6 +67,8 @@ import com.example.foodapp.Foods
 @Composable
 fun MenuScreen(context: Context, bottomBarState: MutableState<Boolean>) {
     val menuNavController = rememberNavController()
+    val sheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable { mutableStateOf(false) }
     NavHost(
         navController = menuNavController,
         startDestination = "Menu"
@@ -144,6 +162,17 @@ fun MenuScreen(context: Context, bottomBarState: MutableState<Boolean>) {
                             .verticalScroll(state),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+                        Surface(
+                            modifier = Modifier
+                                .height(60.dp)
+                                .width(270.dp)
+                        ) {
+                            Button(
+                                onClick = {
+                                    isSheetOpen = true
+                                }) {
+                                Text("Filtry")
+                            } }
                         Column(
                             modifier = Modifier
                                 .height(50.dp)
@@ -261,6 +290,166 @@ fun MenuScreen(context: Context, bottomBarState: MutableState<Boolean>) {
                                 .height(100.dp)
                                 .fillMaxWidth()
                         ) {}
+                        if(isSheetOpen) {
+                            ModalBottomSheet(sheetState = sheetState, onDismissRequest = { isSheetOpen= false })
+                            {
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                        verticalArrangement = Arrangement.Center,
+                                        horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Surface(
+                                        modifier = Modifier
+                                            .height(30.dp)
+                                            .fillMaxWidth()
+                                            .padding(horizontal = 15.dp)
+                                    ) {
+                                        Text(
+                                            text = "Kaloryczność (kcal)",
+                                            fontSize = MaterialTheme.typography.titleLarge.fontSize
+                                        )
+                                    }
+                                    Column(
+                                        modifier = Modifier
+                                            .padding(horizontal = 15.dp, vertical = 10.dp)
+                                            .width(270.dp),
+                                    ) {
+                                        OutlinedTextField(
+                                            value = "",
+                                            onValueChange = {
+
+                                            },
+                                            label = {
+                                                Text(text = "Od")
+                                            },
+                                            singleLine = true,
+                                            keyboardOptions = KeyboardOptions(
+                                                keyboardType = KeyboardType.Number,
+                                                imeAction = ImeAction.Next,
+                                            ),
+                                            trailingIcon = {
+                                                IconButton(onClick = {  }) {
+                                                    Icon(
+                                                        imageVector = Icons.Default.Clear,
+                                                        contentDescription = "Clear button"
+                                                    )
+                                                }
+                                            })
+                                    }
+                                    Column(
+                                        modifier = Modifier
+                                            .padding(horizontal = 15.dp, vertical = 10.dp)
+                                            .width(270.dp),
+                                    ) {
+                                        OutlinedTextField(
+                                            value = "",
+                                            onValueChange = {
+
+                                            },
+                                            label = {
+                                                Text(text = "Do")
+                                            },
+                                            singleLine = true,
+                                            keyboardOptions = KeyboardOptions(
+                                                keyboardType = KeyboardType.Number,
+                                                imeAction = ImeAction.Next,
+                                            ),
+                                            trailingIcon = {
+                                                IconButton(onClick = {  }) {
+                                                    Icon(
+                                                        imageVector = Icons.Default.Clear,
+                                                        contentDescription = "Clear button"
+                                                    )
+                                                }
+                                            })
+                                    }
+                                    Surface(
+                                        modifier = Modifier
+                                            .height(30.dp)
+                                            .fillMaxWidth()
+                                            .padding(horizontal = 15.dp)
+                                    ) {
+                                        Text(
+                                            text = "Porcje (osoby)",
+                                            fontSize = MaterialTheme.typography.titleLarge.fontSize
+                                        )
+                                    }
+                                    Column(
+                                        modifier = Modifier
+                                            .padding(horizontal = 15.dp, vertical = 10.dp)
+                                            .width(270.dp),
+                                    ) {
+                                        OutlinedTextField(
+                                            value = "",
+                                            onValueChange = {
+
+                                            },
+                                            label = {
+                                                Text(text = "Od")
+                                            },
+                                            singleLine = true,
+                                            keyboardOptions = KeyboardOptions(
+                                                keyboardType = KeyboardType.Number,
+                                                imeAction = ImeAction.Next,
+                                            ),
+                                            trailingIcon = {
+                                                IconButton(onClick = {  }) {
+                                                    Icon(
+                                                        imageVector = Icons.Default.Clear,
+                                                        contentDescription = "Clear button"
+                                                    )
+                                                }
+                                            })
+                                    }
+                                    Column(
+                                        modifier = Modifier
+                                            .padding(horizontal = 15.dp, vertical = 10.dp)
+                                            .width(270.dp),
+                                    ) {
+                                        OutlinedTextField(
+                                            value = "",
+                                            onValueChange = {
+
+                                            },
+                                            label = {
+                                                Text(text = "Do")
+                                            },
+                                            singleLine = true,
+                                            keyboardOptions = KeyboardOptions(
+                                                keyboardType = KeyboardType.Number,
+                                                imeAction = ImeAction.Next,
+                                            ),
+                                            trailingIcon = {
+                                                IconButton(onClick = {  }) {
+                                                    Icon(
+                                                        imageVector = Icons.Default.Clear,
+                                                        contentDescription = "Clear button"
+                                                    )
+                                                }
+                                            })
+                                    }
+                                    Surface(
+                                        modifier = Modifier
+                                            .height(60.dp)
+                                            .width(270.dp)
+                                    ) {
+                                        Button(
+                                            onClick = {
+                                                isSheetOpen = false
+                                            }
+                                        ) {
+                                            Text("Filtruj")
+                                        }
+                                    }
+                                    Surface(
+                                        modifier = Modifier
+                                            .height(50.dp)
+                                            .fillMaxWidth()
+                                    ) {}
+                                }
+                            }
+                        }
                     }
                 })
         }
@@ -314,7 +503,7 @@ fun FoodCard(
         ),
         modifier = Modifier
             .size(width = 150.dp, height = 150.dp)
-            .clickable (onClick = onClick)
+            .clickable(onClick = onClick)
     ) {
         Image(
             bitmap = image.asImageBitmap(),
@@ -330,3 +519,4 @@ fun FoodCard(
         )
     }
 }
+
